d49c8a37d757e5f68f6784cab3a83dbd22400693
castares <cesar.castares@gmail.com>
2019-11-30 16:45:40 +0100

    commit

--- api.py
+++ api.py
@@ -1,20 +1,33 @@
-from bottle import request, response
-from bottle import post, get, put, delete
-from mongo import MongoClient
+from bottle import post, get, request, response, run
+import mongodb as mdb
+import bson
+import os
 
 
-### User endpoint
+# User endpoint
 @post("/user/create")
 def createUser():
-    return
+    # extract and validate user
+    try:
+        userid = request.forms.get("idUser")
+        username = request.forms.get('userName')
 
-### Chat endpoint
-@post("/chat/create")
-def createChat():
-    return 
+    except:
+        raise ValueError
+
+    # insert document to mongo collection.
+    return {"inserted_user_id": str(users.newUser(userid, username))}
+
+# # Chat endpoint
+# @post("/chat/create")
+# def createChat():
+#     return
 
 
+def main():
+    users.
+    run(host='localhost', port=8000)
 
 
 if __name__ == '__main__':
-    bottle.run(host ='localhost', port = 8000)
+    main()
--- mongodb.py
+++ mongodb.py
@@ -1,26 +1,62 @@
+#!/usr/bin/python3
+
+import os
+from pymongo import MongoClient
+import getpass
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-#Environment variable: 
-connectionURL = os.getenv("MONGODB_URL")
 
-class CollConection:
+class CollConnection:
 
-    def __init__(self,connectionurl, dbName,collection):
-        self.client = MongoClient(connectionURL)
-        self.db = self.client[dbName]
-        self.collection=self.db[collection]
+    def __init__(self, connectionurl, db, collection):
+        self.client = MongoClient(connectionurl)
+        self.db = self.client[db]
+        self.collection = self.db[collection]
 
-    def addDocument(self,document):
-        a=self.collection.insert_one(document)
+    def addDocument(self, document):
+        a = self.collection.insert_one(document)
         print("Inserted", a.inserted_id)
         return a.inserted_id
-    
-    def addChiste(self, autor, chiste):
-        document={'autor':autor,
-                'chiste':chiste}
+
+    def newUser(self, userid, username):
+        document = {
+            'userid': userid,
+            'username': username
+        }
         return self.addDocument(document)
 
-coll = CollConnection(connectionURL)
+    def newMessage(self, userid, chatid, messageid, datetime, text):
+        document = {
+            'idUser': userid,
+            'idChat': chatid,
+            'idMessage': messageid,
+            'datetime': datetime,
+            'text': text
+        }
+        return self.addDocument(document)
+
+
+def main():
+    # Connect to DB
+    mongodbUrl = os.getenv("MONGODBURL")
+    print(f'Conected to mongodb {mongodbUrl}')
+
+    # define Client, Database and collections on that database
+    client = MongoClient(mongodbUrl)
+    db = client['chatsdb']
+    collist = db.list_collection_names()
+
+    # Create New Collections
+    new_collections = ["Users", "Messages"]
+    for coll in new_collections:
+        if coll not in collist:
+            db.create_collection(coll)
+        else:
+            print(f"Collection {coll} already exists")
+
+
+if __name__ == "__main__":
+    main()
